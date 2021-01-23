1680351bfa99827c2bf6c940dd5ded4ba8b06e11
Iliya Yankow <iliya.yankow@gmail.com>
2021-01-23 19:16:00 +0200

    Testing JUnit

--- TestSrc/com/tddJunit/java/MathsTest.java
+++ TestSrc/com/tddJunit/java/MathsTest.java
@@ -1,35 +1,50 @@
 package com.tddJunit.java;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MathsTest {
 
-	//Comment-1: private Maths classundertest;	
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+	 private Maths classundertest;	
 
 	@Before
 	public void setUp() throws Exception {
 
-		//comment-2: classundertest = new Maths();
+		 classundertest = new Maths();
 
 	}
 
+    @After
+    public void tearDown() throws Exception {
+    }
+
 	@Test
 	public void testAdd() {
-		//comment-3 : fail("Not yet implemented");
+		 //fail("Not yet implemented");
 
-		/* comment-4:	long result = 10+2;
-assertEquals(result, classundertest.add(10,2)); */
+			long result = 10+2;
+assertEquals(result, classundertest.add(10,2));
 
 	}
 
 	@Test
 	public void testSubtract() {
-	//comment-5: fail("Not yet implemented");
-	/* comment-6: long result = 10-2;
-		assertEquals(result, classundertest.subtract(10,2));*/	
+	// fail("Not yet implemented");
+	 long result = 10-2;
+		assertEquals(result, classundertest.subtract(10,2));
 
 	}
 
